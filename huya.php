3338f9b87dc799ee362aa6e0be75199c7da9b91b
wjxgzz <85028773+wjxgzz@users.noreply.github.com>
2022-05-06 13:10:14 +0800

    新版本虎牙代码
    32位php适用

--- huya.php
+++ huya.php
@@ -1,27 +1,9 @@
 <?php
+    // sv=2110211124&uuid算法代码 //
 
-    // huya parse @update 2022.01.15 // 偷鸡版本,暂时能用,什么时候跟移动端同步更新uid算法以后再说.
+    $uid = '1461688433250';
     $rid = $_GET['id'];
-    $cdnType = $_GET['cdn']; //用于指定CDN节点：al/tx/hw/bd/ws [阿里/腾讯/华为/百度/网宿] 
-
-    //$uid = 1234567890123; // 这里测试UID可以固定不变，但需要自己抓包自己的UID,注册的为固定
-    // 匿名获取游客UID
-    $bstrURL = "https://udblgn.huya.com/web/anonymousLogin";
-    $postdata = '{"appId":5002,"byPass":3,"context":"","version":"2.4","data":{}}';
-    $ch = curl_init();
-    curl_setopt($ch,CURLOPT_URL,$bstrURL);
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); 
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-    curl_setopt($ch, CURLOPT_POST,true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS,$postdata);
-    curl_setopt($ch, CURLOPT_HTTPHEADER,["Content-Type: application/json"]);
-    $data = curl_exec($ch);
-    curl_close($ch);
-    $json = json_decode($data);
-
-    $uid = $json->data->uid; 
-
+    $cdnType = $_GET['cdn']; //用于手动指定CDN节点 AL/TX/BD/HW....
     $bstrURL = "https://mp.huya.com/cache.php?m=Live&do=profileRoom&roomid=$rid";
 
     $ch = curl_init();
@@ -36,30 +18,32 @@
     // 组装播放地址
     $sStreamName = $bStreamLst->sStreamName;
     $sAntiCode = $bStreamLst->sHlsAntiCode;
-    
-    //data.stream.hls.multiLine 当前使用线路第一个
+
     $sCdnType = strtolower($json->data->stream->hls->multiLine[0]->cdnType);
-    $sHlsUrl = "http://".(($cdnType=='')?$sCdnType:$cdnType).".hls.huya.com/src/";
+    $sHlsUrl = "https://".(($cdnType=='')?$sCdnType:$cdnType).".hls.huya.com/src/";
 
     parse_str($sAntiCode,$params);
     $fm = base64_decode($params['fm']);
     $wsTime = $params['wsTime'];
-    $msTime = strval(time()).strval(intval(1000000 + mt_rand()/mt_getrandmax()*(9999999-1000000))); // 17位时间戳 32位PHP应用
 
-    //$seqid = $uid + time()*10000000+1234567; // uid(1234567890123) + date.now() == 17位时间戳 // 64位偷鸡
-    $seqid = big_integer_add($uid,$msTime); // 32位PHP环境用这行
-    $t = $params['t'];
-    $ctype = $params['ctype'];
+    $msTime = time().'212';//13位时间戳;
+    $seqid = big_integer_add($uid,$msTime);
+
+    $t = 103;
+    $ctype = 'tars_mobile';
+
     $i = md5($seqid.'|'.$ctype.'|'.$t);
 
     $wsSecret = md5(str_replace(['$0','$1','$2','$3'],[$uid,$sStreamName,$i,$wsTime],$fm));  // uid_streamname_hash_wstime
     
-    $sPlayUrl = $sHlsUrl.$sStreamName.'.m3u8?wsSecret='.$wsSecret.'&wsTime='.$wsTime.'&uuid=&uid='.$uid.'&txyp=o%253Ad2%253B&fs=bgct&sphdcdn=al_7-tx_3-js_3-ws_7-bd_2-hw_2&sphdDC=huya&sphd=264_*-265_*&ctype='.$ctype.'&seqid='.$seqid.'&ver=1&t='.$t;
-    
-    header("location:$sPlayUrl");
+    $uuid = Kmod(substr($msTime,3,10).'212',4294967295);// % 4294967295;
 
-    // 32位PHP 大数计算法，64位不需要
-    function big_integer_add($num1,$num2){
+    $sPlayUrl = $sHlsUrl.$sStreamName.'.m3u8?wsSecret='.$wsSecret.'&wsTime='.$wsTime.'&uuid='.$uuid.'&uid='.$uid.'&txyp=o%253Ad2%253B&fs=bgct&sphdcdn=al_7-tx_3-js_3-ws_7-bd_2-hw_2&sphdDC=huya&sphd=264_*-265_*&ctype='.$ctype.'&seqid='.$seqid.'&ver=1&t='.$t.'&sv=2110211124';
+
+    header("location:".$sPlayUrl);
+
+    function big_integer_add($num1,$num2) // 32位PHP大数加法
+    {
         $str1 = strval($num1);
         $str2 = strval($num2);
         $len2 = strlen($str1);
@@ -80,5 +64,10 @@
         }
         return $result;
     }
-
+    
+    
+    function Kmod($n1, $n2) // 32位PHP大数取余 
+    {
+        return intval(fmod(floatval($n1), $n2));
+    }
 ?>
