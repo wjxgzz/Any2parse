97609d1b0ae25832ae8f22134a955d8ef83c3149
wjxgzz <85028773+wjxgzz@users.noreply.github.com>
2022-07-15 01:10:15 +0800

    douyin直播
    免签免验证接口

---
+++ dylive.php
@@ -0,0 +1,18 @@
+<?php
+    // xxx要的douyin免签免验证接口 .需要长id
+    
+    $id = $_GET['id']; // 7120247098448153352
+    $bstrURL = "https://webcast.amemv.com/webcast/room/reflow/info/?type_id=0&live_id=1&room_id={$id}&sec_user_id=&app_id=1128";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $bstrURL);	 	 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['User-Agent: Mobile Safari']);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); 
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE); 
+    $data = curl_exec($ch);
+    curl_close($ch);
+    $json = json_decode($data);
+    $playURL = $json->data->room->stream_url->hls_pull_url_map->FULL_HD1;
+    header('location:'.$playURL);
+
+?>
