aae99b071c1096aff1ce00b3225fe888aef2779c
wjxgzz <85028773+wjxgzz@users.noreply.github.com>
2023-09-06 22:41:48 +0800

    重庆有线切片版本
    重庆有线切片版本

---
+++ cqyx_q.php
@@ -0,0 +1,159 @@
+<?php
+
+    // 重庆有线切片版本
+    // ps: 本来不想写的，因为有人写过，但又有私信求的
+    // 写的不好，仅供参考
+
+    $ids =[
+        "cctv1HD"=>["4","a48k","1"],
+        "cctv2HD"=>["6","a48k","1"],
+        "cctv3HD"=>["6","a48k","1"],
+        "cctv4HD"=>["6","a48k","3"],
+        "cctv5HD"=>["6","a48k","1"],
+        "cctv5SportHD"=>["6","a48k","1"],
+        "cctv6HD"=>["6","a48k","1"],
+        "cctv7HD"=>["6","a48k","3"],
+        "cctv8HD"=>["6","a48k","1"],
+        "cctv9HD"=>["6","a48k","1"],
+        "cctv10HD"=>["6","a48k","1"],
+        "cctv11HD"=>["6","a48k_zho","1"],
+        "cctv12HD"=>["6","a48k","3"],
+        "CCTVNewsHD"=>["4","a48k","1"],
+        "cctvseHD"=>["6","a48k","1"],
+        "cctv15HD"=>["6","a48k_zho","1"],
+        "cctv16HD"=>["6","a48k_zho","1"],
+        "cctv17HD"=>["6","a48k","2"],
+        "CCTV4K"=>["15","a48k_zho","1"],
+        "hxjc_4K"=>["15","a48k_zho","1"],
+        "CCTV16_4K"=>["15","a48k_zho","1"],
+        "jiangsuHD"=>["6","a48k_zho","1"],
+        "beijingHD"=>["6","a48k","10"],
+        "zhejiangHD"=>["6","a48k","5"],
+        "shanghaiHD"=>["6","a48k","3"],
+        "shenzhenHD"=>["6","a48k","4"],
+        "liaoningHD"=>["6","a48k","6"],
+        "tianjinHD"=>["6","a48k","4"],
+        "guizhouHD"=>["6","a48k_zho","1"],
+        "sichuanHD"=>["6","a48k","2"],
+        "chongqingHD"=>["4","a48k","1"],
+        "yunnanSD"=>["4","a48k","3"],
+        "heilongjiangHD"=>["4","a48k","3"],
+        "anhuiSD"=>["4","a48k","3"],
+        "hebeiSD"=>["4","a48k","3"],
+        "jiangxiHD"=>["4","a48k_zho","5"],
+        "hubeiSD"=>["4","a48k","3"],
+        "hainanSD"=>["4","a48k","3"],
+        "dongnanHD"=>["4","a48k","3"],
+        "jilinHD"=>["6","a48k","4"],
+        "guangxiHD"=>["6","a48k_zho","1"],
+        "neimengkuSD"=>["2","a48k","3"],
+        "hunanHD"=>["6","a48k","4"],
+        "guangdongHD"=>["6","a48k","2"],
+        "henanHD"=>["6","a48k","1"],
+        "shanxiSD"=>["2","a48k","1"],
+        "bingtuanSD"=>["2","a48k","1"],
+        "ningxia"=>["2","a48k_zho","1"],
+        "bjkakuSD"=>["2","a48k","1"],
+        "jinyingSD"=>["6","a48k","2"],
+        "sanshaSD"=>["2","a48k","1"],
+        "qinghaiSD"=>["2","a48k","1"],
+        "tianqiSD"=>["2","a48k","1"],
+        "bjayjsSD"=>["6","a48k","1"],
+        "sdjiaoyuSD"=>["2","a48k","1"],
+        "xinjiangSD"=>["2","a48k","1"],
+        "xizangSD"=>["2","a48k","1"],
+        "cgtnSD"=>["2","a48k","1"],
+        "cetv-1SD"=>["2","a48k","1"],
+        "cetv-4SD"=>["2","a48k","1"],
+        "shenghuo"=>["2","a48k_zho","1"],
+        "jiankangSD"=>["2","a48k","1"],
+        "mryyHD"=>["6","a48k_zho","1"],
+        "xfjcHD"=>["6","a48k_zho","1"],
+        "klcdHD"=>["6","a48k_zho","1"],
+        "diyijuchangHD"=>["4","a48k","2"],
+        "dsjcHD"=>["6","a48k_zho","1"],
+        "dmxcHD"=>["6","a48k_zho","1"],
+        "mlzqHD"=>["6","a48k_zho","1"],
+        "fyjcHD"=>["6","a48k_zho","1"],
+        "qsjlHD"=>["6","a48k_zho","1"],
+        "bqkjHD"=>["6","a48k_zho","1"],
+        "yxfyHD"=>["6","a48k_zho","1"],
+        "chcgqdyHD"=>["6","a48k_zho","1"],
+        "chcjtyyHD"=>["6","a48k_zho","1"],
+        "chcdzdyHD"=>["6","a48k_zho","1"],
+        "xfylHD"=>["6","a48k_zho","1"],
+        "fsshHD"=>["6","a48k_zho","1"],
+        "hjjcHD"=>["6","a48k_zho","1"],
+        "fyyyHD"=>["6","a48k_zho","1"],
+        "ystqHD"=>["6","a48k_zho","1"],
+        "fyzqHD"=>["6","a48k_zho","1"],
+        "yswhHD"=>["6","a48k_zho","1"],
+        "golfHD"=>["6","a48k_zho","1"],
+        "zaojiaoHD"=>["6","a48k_zho","1"],
+        "dfcj"=>["2","a48k_zho","1"],
+        "jiazheng"=>["2","a48k_zho","1"],
+        "shuhua"=>["2","a48k_zho","1"],
+        "dongman"=>["2","a48k_zho","1"],
+        "guoxue"=>["2","a48k_zho","1"],
+        "youxi"=>["2","a48k_zho","1"],
+        "techan"=>["2","a48k_zho","1"],
+        "car"=>["2","a48k_zho","1"],
+        "shengtai"=>["2","a48k_zho","1"],
+        "diaoyu"=>["2","a48k_zho","1"],
+        "faxian"=>["2","a48k_zho","1"],
+        "doxtv"=>["2","a48k_zho","1"],
+        "fazhi"=>["2","a48k_zho","1"],
+        "jinse"=>["2","a48k_zho","1"],
+        "soucang"=>["2","a48k_zho","1"],
+        "caifu"=>["2","a48k_zho","1"],
+        "nvxing"=>["2","a48k_zho","1"],
+        "qicai"=>["2","a48k_zho","1"],
+        "weiqi"=>["2","a48k_zho","1"],
+        "xianfeng"=>["2","a48k_zho","1"],
+        "huanqiulvyou"=>["2","a48k","1"],
+        "gushi"=>["2","a48k_zho","1"],
+        "sheying"=>["2","a48k_zho","1"],
+        "CQTVNewsHD"=>["4","a48k","1"],
+        "cqyingshiHD"=>["6","a48k","1"],
+        "CQTVTrendyHD"=>["6","a48k","1"],
+        "cqssgwHD"=>["6","a48k","1"],
+        "cqwtylHD"=>["6","a48k","1"],
+        "cqggncHD"=>["6","a48k","1"],
+        "cqseHD"=>["6","a48k","1"],
+        "CQTVkejiaoHD"=>["6","a48k_zho","1"],
+        "cqcarSD"=>["2","a48k","1"],
+        "aikanHD"=>["6","a48k_zho","2"],
+        "changshou"=>["2","a48k","1"],
+        "youyang"=>["2","a48k","2"],
+        "cqrongmei"=>["2","a48k","1"],
+
+    ];
+    $req = basename(parse_url($_SERVER['REQUEST_URI'])["path"]); // 这里可以选择写死文件名 如： cqyx_q.php
+    $id = $_GET['id']?:'cctv1HD';
+    $type = $_GET['type']?:'playlist'; // audio // video
+    $seq = $_GET['seq']?:intval(time()/10)-1080;
+    $content = "#EXTM3U\n";
+    if($type == 'playlist')
+    {
+        $content.= "#EXT-X-VERSION:4\n#EXT-X-INDEPENDENT-SEGMENTS\n";
+        $content.= "#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID=\"cqyxaudio\",DEFAULT=YES,URI=\"{$req}?id={$id}&type=audio&seq={$seq}\"\n";
+        $content.= "#EXT-X-STREAM-INF:BANDWIDTH=".strval($ids[$id][0]*1024*1024).",AUDIO=\"cqyxaudio\"\n";
+        $content.= "{$req}?id={$id}&type=video&seq={$seq}\n";
+    }
+    else
+    {
+        $content.= "#EXT-X-VERSION:3\n#EXT-X-MEDIA-SEQUENCE:{$seq}\n#EXT-X-TARGETDURATION:11\n";
+        $path = $ids[$id][2]."/".($type=="audio"?$ids[$id][1]:"v".$ids[$id][0]."M");
+        for($i=0;$i<6;$i++) // 切片数量你酌情修改，此处定义数量为 6 
+        {
+            
+            $content .= "#EXTINF:10.000,\n";
+            $content .= "http://hls.live.cqccn.com/__cl/cg:live/__c/{$id}/__op/default/__f/{$path}/".strval($seq+$i).".ts\n";
+        }
+    }
+
+    header("Content-Type: application/vnd.apple.mpegURL");
+    header("Content-Disposition: filename={$id}.m3u8");
+    echo $content;  
+
+?>
